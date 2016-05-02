30d23b9703a3c6209c822aa45b0f11c8e4a85f5b
OliGon <kaithmenken@gmail.com>
2016-05-02 13:15:07 +0000

    changes addLine to two seperate functions for grey and coloured image

--- src/svg-files.hpp
+++ src/svg-files.hpp
@@ -5,7 +5,8 @@
 #include "line-struct.hpp"
 
 void initSvg(std::fstream &outFile, int height, int width, bool blackBackground, int lineOpacity, int lineThickness);
-void addLine(std::fstream &outFile, LineData lineCoords);
+void addLineGrey(std::fstream &outFile, LineData lineCoords);
+void addLineCol(std::fstream &outFile, LineData lineCoords, std::string colour);
 void finishSvg(std::fstream &outFile);
 
 #endif // SVG_FILES_HPP
