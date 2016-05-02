ef3da2b47b47dd73f6730d81b566566e42e9edb5
OliGon <kaithmenken@gmail.com>
2016-05-02 12:08:59 +0000

    declared header-file for svg-files

--- src/svg-files.hpp
+++ src/svg-files.hpp
@@ -4,8 +4,8 @@
 #include <fstream>
 #include "line-struct.hpp"
 
-
-
-
+void initSvg(std::fstream &outFile, int height, int width, bool blackBackground, int lineOpacity, int lineThickness);
+void addLine(std::fstream &outFile, LineData lineCoords);
+void finishSvg(std::fstream &outFile);
 
 #endif // SVG_FILES_HPP
