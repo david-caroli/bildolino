c5dde22e7a90103a634100616b2a573db3fb6d14
kaith-uwe-menken <kaith-uwe.menken@uni-ulm.de>
2016-05-17 14:59:01 +0200

    fixed conversion-bug at lineOpacity. converted lineOpacity to float

--- src/svg-files.cpp
+++ src/svg-files.cpp
@@ -1,7 +1,7 @@
 #include "svg-files.hpp"
 
 void initSvg(std::fstream &outFile, int height, int width, bool blackBackground, int lineOpacity, int lineThickness){
-	outFile << "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width = \"" << width << "\" height = \"" << height << "\" style = \"background:" << (blackBackground ? "black":"white") << "\">\n\t<defs>\n\t\t<style type=\"text/css\"><![CDATA[\n\t\t\tline{\n\t\t\t\tstroke-width:"<< lineThickness <<" ;\n\t\t\t\tstroke-opacity:"<< (lineOpacity/255) <<" ;\n\t\t\t}\n\t\t]]></style>\n\t</defs>\n";
+	outFile << "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width = \"" << width << "\" height = \"" << height << "\" style = \"background:" << (blackBackground ? "black":"white") << "\">\n\t<defs>\n\t\t<style type=\"text/css\"><![CDATA[\n\t\t\tline{\n\t\t\t\tstroke-width:"<< lineThickness <<" ;\n\t\t\t\tstroke-opacity:"<< ((float)lineOpacity/255) <<" ;\n\t\t\t}\n\t\t]]></style>\n\t</defs>\n";
 }
 
 void addLineGrey(std::fstream &outFile, LineData lineCoords){
