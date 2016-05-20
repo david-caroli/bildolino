61dcfee336e126d354f477bba90681f3b0f02f2f
kaith-uwe-menken <kaith-uwe.menken@uni-ulm.de>
2016-05-20 16:15:09 +0200

    fixed svg background being transparent. root-element does not get rendered, neither its background-color. thus we needed a rectangle functioning as background. fixed stroke-color being always black by using inverted background color

--- src/svg-files.cpp
+++ src/svg-files.cpp
@@ -1,11 +1,21 @@
 #include "svg-files.hpp"
 
 void initSvg(std::fstream &outFile, int height, int width, bool blackBackground, int lineOpacity, int lineThickness){
-	outFile << "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width = \"" << width << "\" height = \"" << height << "\" style = \"background:" << (blackBackground ? "black":"white") << "\">\n\t<defs>\n\t\t<style type=\"text/css\"><![CDATA[\n\t\t\tline{\n\t\t\t\tstroke-width:"<< lineThickness <<" ;\n\t\t\t\tstroke-opacity:"<< ((float)lineOpacity/255) <<" ;\n\t\t\t}\n\t\t]]></style>\n\t</defs>\n";
+	outFile << "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width = \"" << width << "\" height = \"" << height << "\">"
+	outFile << "\n\t<defs>"
+	outFile << "\n\t\t<style type=\"text/css\"><![CDATA["
+	outFile << "\n\t\t\tline{"
+	outFile << "\n\t\t\t\tstroke-width:"<< lineThickness <<" ;"
+	outFile << "\n\t\t\t\tstroke-opacity:"<< ((float)lineOpacity/255) <<" ;"
+	outFile << "\n\t\t\t\tstroke:"<< (blackBackground ? "white":"black") <<" ;"
+	outFile << "\n\t\t\t}"
+	outFile << "\n\t\t]]></style>"
+	outFile << "\n\t</defs>"
+	outFile << "\n<rect width = \""<< width <<"\" height = \""<< height <<"\" fill=\""<< (blackBackground ? "black":"white") <<"\"/>\n";
 }
 
 void addLineGrey(std::fstream &outFile, LineData lineCoords){
-	outFile << "	<line x1=\"" << lineCoords.a.x << "\" y1=\"" << lineCoords.a.y << "\" x2=\"" << lineCoords.b.x << "\" y2=\"" << lineCoords.b.y << "\" stroke=\"black\"/>\n";
+	outFile << "	<line x1=\"" << lineCoords.a.x << "\" y1=\"" << lineCoords.a.y << "\" x2=\"" << lineCoords.b.x << "\" y2=\"" << lineCoords.b.y << "\"/>\n";
 }
 
 void addLineCol(std::fstream &outFile, LineData lineCoords, std::string colour){
