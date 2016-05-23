7e37c466def910ee03369fb8e0d29dd3e2d2ed93
david-caroli <david.caroli@mail.de>
2016-05-23 20:46:03 +0200

    changed scoring to use averages. implemented color mode.

--- src/bresenham-line-algorithms.cpp
+++ src/bresenham-line-algorithms.cpp
@@ -3,6 +3,12 @@
 
 
 
+
+
+
+
+
+
 void drawGrayscaleLine(boost::gil::gray8_view_t &img, LineData line, int opacity, bool additive) {
 	if(abs(line.b.y - line.a.y) > abs(line.b.x - line.a.x)) {
 		std::swap(line.a.x, line.a.y);
@@ -125,6 +131,7 @@ void drawGrayscaleLineSteepAngleSubtractive(boost::gil::gray8_view_t &img, LineD
 
 
 
+
 unsigned long grayscaleLineScore(boost::gil::gray8_view_t &img, LineData line, int opacity, bool additive) {
 	if(abs(line.b.y - line.a.y) > abs(line.b.x - line.a.x)) {
 		std::swap(line.a.x, line.a.y);
@@ -142,7 +149,6 @@ unsigned long grayscaleLineScore(boost::gil::gray8_view_t &img, LineData line, i
 		}
 	}
 }
-
 unsigned long grayscaleLineScoreNormalAngleAdditive(boost::gil::gray8_view_t &img, LineData line, int opacity) {
 	if(line.a.x > line.b.x)
 		std::swap(line.a, line.b);
@@ -153,7 +159,7 @@ unsigned long grayscaleLineScoreNormalAngleAdditive(boost::gil::gray8_view_t &im
 	int ystep = (line.a.y < line.b.y) ? 1 : -1;
 	int y = line.a.y;
 	const int maxX = 1+line.b.x;
-	unsigned long score = 0;
+	unsigned long score=0, pixelCount=0;
 
 	for(int x=line.a.x; x<maxX; x++) {
 		if(img(x,y) < opacity) {
@@ -166,8 +172,9 @@ unsigned long grayscaleLineScoreNormalAngleAdditive(boost::gil::gray8_view_t &im
 			y += ystep;
 			error += dx;
 		}
+		++pixelCount;
 	}
-	return score;
+	return score/pixelCount;
 }
 
 unsigned long grayscaleLineScoreNormalAngleSubtractive(boost::gil::gray8_view_t &img, LineData line, int opacity) {
@@ -180,7 +187,7 @@ unsigned long grayscaleLineScoreNormalAngleSubtractive(boost::gil::gray8_view_t 
 	int ystep = (line.a.y < line.b.y) ? 1 : -1;
 	int y = line.a.y;
 	const int maxX = 1+line.b.x;
-	unsigned long score = 0;
+	unsigned long score=0, pixelCount=0;
 
 	for(int x=line.a.x; x<maxX; x++) {
 		if((255-img(x,y)) < opacity) { // potentially optimize to if(img(x,y) > invertedOpacity)   where invertedOpacity=255-opacity
@@ -193,8 +200,9 @@ unsigned long grayscaleLineScoreNormalAngleSubtractive(boost::gil::gray8_view_t 
 			y += ystep;
 			error += dx;
 		}
+		++pixelCount;
 	}
-	return score;
+	return score/pixelCount;
 }
 
 unsigned long grayscaleLineScoreSteepAngleAdditive(boost::gil::gray8_view_t &img, LineData line, int opacity) {
@@ -207,7 +215,7 @@ unsigned long grayscaleLineScoreSteepAngleAdditive(boost::gil::gray8_view_t &img
 	int ystep = (line.a.y < line.b.y) ? 1 : -1;
 	int y = line.a.y;
 	const int maxX = 1+line.b.x;
-	unsigned long score = 0;
+	unsigned long score=0, pixelCount=0;
 
 	for(int x=line.a.x; x<maxX; x++) {
 		if(img(y,x) < opacity) {
@@ -220,8 +228,9 @@ unsigned long grayscaleLineScoreSteepAngleAdditive(boost::gil::gray8_view_t &img
 			y += ystep;
 			error += dx;
 		}
+		++pixelCount;
 	}
-	return score;
+	return score/pixelCount;
 }
 
 unsigned long grayscaleLineScoreSteepAngleSubtractive(boost::gil::gray8_view_t &img, LineData line, int opacity) {
@@ -234,7 +243,7 @@ unsigned long grayscaleLineScoreSteepAngleSubtractive(boost::gil::gray8_view_t &
 	int ystep = (line.a.y < line.b.y) ? 1 : -1;
 	int y = line.a.y;
 	const int maxX = 1+line.b.x;
-	unsigned long score = 0;
+	unsigned long score=0, pixelCount=0;
 
 	for(int x=line.a.x; x<maxX; x++) {
 		if((255-img(y,x)) < opacity) { // potentially optimize to if(img(y,x) > invertedOpacity)   where invertedOpacity=255-opacity
@@ -247,7 +256,267 @@ unsigned long grayscaleLineScoreSteepAngleSubtractive(boost::gil::gray8_view_t &
 			y += ystep;
 			error += dx;
 		}
+		++pixelCount;
 	}
-	return score;
+	return score/pixelCount;
+}
+
+
+
+
+
+
+
+
+
+
+void drawRGBLine(boost::gil::rgb8_view_t &img, LineData line, int opacity, bool additive, int channel) {
+	if(abs(line.b.y - line.a.y) > abs(line.b.x - line.a.x)) {
+		std::swap(line.a.x, line.a.y);
+		std::swap(line.b.x, line.b.y);
+		if(additive) {
+			drawRGBLineSteepAngleAdditive(img, line, opacity, channel);
+		} else {
+			drawRGBLineSteepAngleSubtractive(img, line, opacity, channel);
+		}
+	} else {
+		if(additive) {
+			drawRGBLineNormalAngleAdditive(img, line, opacity, channel);
+		} else {
+			drawRGBLineNormalAngleSubtractive(img, line, opacity, channel);
+		}
+	}
+}
+void drawRGBLineNormalAngleAdditive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c) {
+	if(line.a.x > line.b.x)
+		std::swap(line.a, line.b);
+
+	float dx = line.b.x - line.a.x;
+	float dy = abs(line.b.y - line.a.y);
+	float error = dx / 2.0f;
+	int ystep = (line.a.y < line.b.y) ? 1 : -1;
+	int y = line.a.y;
+	const int maxX = 1+line.b.x;
+
+	for(int x=line.a.x; x<maxX; x++) {
+		if(img(x,y)[c] < (255-opacity)) {
+			img(x,y)[c] = img(x,y)[c]+opacity;
+		} else {
+			img(x,y)[c] = 255;
+		}
+		error -= dy;
+		if(error < 0) {
+			y += ystep;
+			error += dx;
+		}
+	}
+}
+
+void drawRGBLineSteepAngleAdditive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c) {
+	if(line.a.x > line.b.x)
+		std::swap(line.a, line.b);
+
+	float dx = line.b.x - line.a.x;
+	float dy = abs(line.b.y - line.a.y);
+	float error = dx / 2.0f;
+	int ystep = (line.a.y < line.b.y) ? 1 : -1;
+	int y = line.a.y;
+	const int maxX = 1+line.b.x;
+
+	for(int x=line.a.x; x<maxX; x++) {
+		if(img(y,x)[c] < (255-opacity)) {
+			img(y,x)[c] = img(y,x)[c]+opacity;
+		} else {
+			img(y,x)[c] = 255;
+		}
+		error -= dy;
+		if(error < 0) {
+			y += ystep;
+			error += dx;
+		}
+	}
+}
+
+void drawRGBLineNormalAngleSubtractive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c) {
+	if(line.a.x > line.b.x)
+		std::swap(line.a, line.b);
+
+	float dx = line.b.x - line.a.x;
+	float dy = abs(line.b.y - line.a.y);
+	float error = dx / 2.0f;
+	int ystep = (line.a.y < line.b.y) ? 1 : -1;
+	int y = line.a.y;
+	const int maxX = 1+line.b.x;
+
+	for(int x=line.a.x; x<maxX; x++) {
+		if(img(x,y)[c] >= opacity) {
+			img(x,y)[c] = img(x,y)[c] - opacity;
+		} else {
+			img(x,y)[c] = 0;
+		}
+		error -= dy;
+		if(error < 0) {
+			y += ystep;
+			error += dx;
+		}
+	}
+}
+
+void drawRGBLineSteepAngleSubtractive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c) {
+	if(line.a.x > line.b.x)
+		std::swap(line.a, line.b);
+
+	float dx = line.b.x - line.a.x;
+	float dy = abs(line.b.y - line.a.y);
+	float error = dx / 2.0f;
+	int ystep = (line.a.y < line.b.y) ? 1 : -1;
+	int y = line.a.y;
+	const int maxX = 1+line.b.x;
+
+	for(int x=line.a.x; x<maxX; x++) {
+		if(img(y,x)[c] >= opacity) {
+			img(y,x)[c] = img(y,x)[c] - opacity;
+		} else {
+			img(y,x)[c] = 0;
+		}
+		error -= dy;
+		if(error < 0) {
+			y += ystep;
+			error += dx;
+		}
+	}
+}
+
+
+
+
+
+unsigned long rgbLineScore(boost::gil::rgb8_view_t &img, LineData line, int opacity, bool additive, int channel) {
+	if(abs(line.b.y - line.a.y) > abs(line.b.x - line.a.x)) {
+		std::swap(line.a.x, line.a.y);
+		std::swap(line.b.x, line.b.y);
+		if(additive) {
+			return rgbLineScoreSteepAngleAdditive(img, line, opacity, channel);
+		} else {
+			return rgbLineScoreSteepAngleSubtractive(img, line, opacity, channel);
+		}
+	} else {
+		if(additive) {
+			return rgbLineScoreNormalAngleAdditive(img, line, opacity, channel);
+		} else {
+			return rgbLineScoreNormalAngleSubtractive(img, line, opacity, channel);
+		}
+	}
+}
+unsigned long rgbLineScoreNormalAngleAdditive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c) {
+	if(line.a.x > line.b.x)
+		std::swap(line.a, line.b);
+
+	float dx = line.b.x - line.a.x;
+	float dy = abs(line.b.y - line.a.y);
+	float error = dx / 2.0f;
+	int ystep = (line.a.y < line.b.y) ? 1 : -1;
+	int y = line.a.y;
+	const int maxX = 1+line.b.x;
+	unsigned long score=0, pixelCount=0;
+
+	for(int x=line.a.x; x<maxX; x++) {
+		if(img(x,y)[c] < opacity) {
+			score += img(x,y)[c];
+		} else {
+			score += opacity;
+		}
+		error -= dy;
+		if(error < 0) {
+			y += ystep;
+			error += dx;
+		}
+		++pixelCount;
+	}
+	return score/pixelCount;
+}
+
+unsigned long rgbLineScoreNormalAngleSubtractive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c) {
+	if(line.a.x > line.b.x)
+		std::swap(line.a, line.b);
+
+	float dx = line.b.x - line.a.x;
+	float dy = abs(line.b.y - line.a.y);
+	float error = dx / 2.0f;
+	int ystep = (line.a.y < line.b.y) ? 1 : -1;
+	int y = line.a.y;
+	const int maxX = 1+line.b.x;
+	unsigned long score=0, pixelCount=0;
+
+	for(int x=line.a.x; x<maxX; x++) {
+		if((255-img(x,y)[c]) < opacity) { // potentially optimize to if(img(x,y) > invertedOpacity)   where invertedOpacity=255-opacity
+			score += (255-img(x,y)[c]);
+		} else {
+			score += opacity;
+		}
+		error -= dy;
+		if(error < 0) {
+			y += ystep;
+			error += dx;
+		}
+		++pixelCount;
+	}
+	return score/pixelCount;
+}
+
+unsigned long rgbLineScoreSteepAngleAdditive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c) {
+	if(line.a.x > line.b.x)
+		std::swap(line.a, line.b);
+
+	float dx = line.b.x - line.a.x;
+	float dy = abs(line.b.y - line.a.y);
+	float error = dx / 2.0f;
+	int ystep = (line.a.y < line.b.y) ? 1 : -1;
+	int y = line.a.y;
+	const int maxX = 1+line.b.x;
+	unsigned long score=0, pixelCount=0;
+
+	for(int x=line.a.x; x<maxX; x++) {
+		if(img(y,x)[c] < opacity) {
+			score += img(y,x)[c];
+		} else {
+			score += opacity;
+		}
+		error -= dy;
+		if(error < 0) {
+			y += ystep;
+			error += dx;
+		}
+		++pixelCount;
+	}
+	return score/pixelCount;
+}
+
+unsigned long rgbLineScoreSteepAngleSubtractive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c) {
+	if(line.a.x > line.b.x)
+		std::swap(line.a, line.b);
+
+	float dx = line.b.x - line.a.x;
+	float dy = abs(line.b.y - line.a.y);
+	float error = dx / 2.0f;
+	int ystep = (line.a.y < line.b.y) ? 1 : -1;
+	int y = line.a.y;
+	const int maxX = 1+line.b.x;
+	unsigned long score=0, pixelCount=0;
+
+	for(int x=line.a.x; x<maxX; x++) {
+		if((255-img(y,x)[c]) < opacity) { // potentially optimize to if(img(y,x) > invertedOpacity)   where invertedOpacity=255-opacity
+			score += (255-img(y,x)[c]);
+		} else {
+			score += opacity;
+		}
+		error -= dy;
+		if(error < 0) {
+			y += ystep;
+			error += dx;
+		}
+		++pixelCount;
+	}
+	return score/pixelCount;
 }
 
--- src/bresenham-line-algorithms.hpp
+++ src/bresenham-line-algorithms.hpp
@@ -6,7 +6,6 @@
 #include "line-struct.hpp"
 
 
-
 // uses the bresenham algorithm to draw a 1 pixel grayscale line on the image view img
 void drawGrayscaleLine(boost::gil::gray8_view_t &img, LineData line, int opacity, bool additive);
 void drawGrayscaleLineNormalAngleAdditive(boost::gil::gray8_view_t &img, LineData line, int opacity);
@@ -20,6 +19,28 @@ unsigned long grayscaleLineScoreNormalAngleAdditive(boost::gil::gray8_view_t &im
 unsigned long grayscaleLineScoreSteepAngleAdditive(boost::gil::gray8_view_t &img, LineData line, int opacity);
 unsigned long grayscaleLineScoreNormalAngleSubtractive(boost::gil::gray8_view_t &img, LineData line, int opacity);
 unsigned long grayscaleLineScoreSteepAngleSubtractive(boost::gil::gray8_view_t &img, LineData line, int opacity);
+
+// uses the bresenham algorithm to draw a 1 pixel rgb line on the image view img
+void drawRGBLine(boost::gil::rgb8_view_t &img, LineData line, int opacity, bool additive, int channel);
+void drawRGBLineNormalAngleAdditive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c);
+void drawRGBLineSteepAngleAdditive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c);
+void drawRGBLineNormalAngleSubtractive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c);
+void drawRGBLineSteepAngleSubtractive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c);
+
+// uses the bresenham algorithm to calculate a score of how much a drawn 1 pixel rgb line would change the image
+unsigned long rgbLineScore(boost::gil::rgb8_view_t &img, LineData line, int opacity, bool additive, int channel);
+unsigned long rgbLineScoreNormalAngleAdditive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c);
+unsigned long rgbLineScoreSteepAngleAdditive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c);
+unsigned long rgbLineScoreNormalAngleSubtractive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c);
+unsigned long rgbLineScoreSteepAngleSubtractive(boost::gil::rgb8_view_t &img, LineData line, int opacity, int c);
+
+
+
+
+
+
+
+
 
 
 
